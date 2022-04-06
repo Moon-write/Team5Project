a62350b869a0e6c18e9ebcf994190e9e02b7f033
Jun <Jun@DESKTOP-5L7DMDT>
2022-04-06 10:08:47 +0900

    Member Vo

---
+++ Team5Project/src/main/java/kr/co/iei/member/vo/Member.java
@@ -0,0 +1,102 @@
+package kr.co.iei.member.vo;
+
+public class Member {
+	private int memberNo;
+	private String memberId;
+	private String memberPw;
+	private String memberName;
+	private String memberNickname;
+	private int memberLevel;
+	private String phone;
+	private String address;
+	private String enrollDate;
+	private String memberGender;
+	private String email;
+	public Member() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Member(int memberNo, String memberId, String memberPw, String memberName, String memberNickname,
+			int memberLevel, String phone, String address, String enrollDate, String memberGender, String email) {
+		super();
+		this.memberNo = memberNo;
+		this.memberId = memberId;
+		this.memberPw = memberPw;
+		this.memberName = memberName;
+		this.memberNickname = memberNickname;
+		this.memberLevel = memberLevel;
+		this.phone = phone;
+		this.address = address;
+		this.enrollDate = enrollDate;
+		this.memberGender = memberGender;
+		this.email = email;
+	}
+	public int getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberPw() {
+		return memberPw;
+	}
+	public void setMemberPw(String memberPw) {
+		this.memberPw = memberPw;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public String getMemberNickname() {
+		return memberNickname;
+	}
+	public void setMemberNickname(String memberNickname) {
+		this.memberNickname = memberNickname;
+	}
+	public int getMemberLevel() {
+		return memberLevel;
+	}
+	public void setMemberLevel(int memberLevel) {
+		this.memberLevel = memberLevel;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEnrollDate() {
+		return enrollDate;
+	}
+	public void setEnrollDate(String enrollDate) {
+		this.enrollDate = enrollDate;
+	}
+	public String getMemberGender() {
+		return memberGender;
+	}
+	public void setMemberGender(String memberGender) {
+		this.memberGender = memberGender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
